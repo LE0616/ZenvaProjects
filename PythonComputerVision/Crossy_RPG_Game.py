80565987eb4b2684a6691b3f490a430177473b5d
LE (Lee) <lm61613@gmail.com>
2019-02-24 14:19:24 -0500

    Game Dev 0.2

--- PythonComputerVision/Crossy_RPG_Game.py
+++ PythonComputerVision/Crossy_RPG_Game.py
@@ -9,7 +9,10 @@ SCREEN_HEIGHT = 800
 SCREEN_TITLE = 'Crossy RPG'
 WHITE_COLOR = (255, 255, 255)
 BLACK_COLOR = (0, 0, 0)
+YELLOW_COLOR = (255, 255, 0)
 clock = pygame.time.Clock()
+pygame.font.init()
+font = pygame.font.SysFont('comicsans', 50)
 
 # Pygame dev 4
 # Making code Obj Oriented with classes and objects
@@ -18,7 +21,7 @@ class Game:
   TICK_RATE = 60
   # FPS Frames Per Second
 
-  def __init__(self, title, width, height):
+  def __init__(self, image_path, title, width, height):
     self.title = title
     self.width = width
     self.height = height
@@ -27,11 +30,17 @@ class Game:
     self.game_screen.fill(WHITE_COLOR)
     pygame.display.set_caption(title)
 
+    background_image = pygame.image.load(image_path)
+    self.image = pygame.transform.scale(background_image, (width, height))
+
   def run_game_loop(self):
     is_game_over = False
+    did_win = False
     direction = 0
 
     player_character = PlayerCharacter('player.png', 375, 700, 50, 50)
+    enemy_0 = EnemyCharacter('enemy.png', 20, 400, 50, 50)
+    treasure = GameObject('treasure.png', 375, 50, 50, 50)
     # Pygame dev 2
     # set up the game loop to render graphics
 
@@ -57,12 +66,43 @@ class Game:
         # print(event)
 
       self.game_screen.fill(WHITE_COLOR)
-      print('direction: ', direction)
-      player_character.move(direction)
+      self.game_screen.blit(self.image, (0, 0))
+
+      treasure.draw(self.game_screen)
+
+      player_character.move(direction, self.height)
       player_character.draw(self.game_screen)
+
+      enemy_0.move(self.width)
+      enemy_0.draw(self.game_screen)
+
+      # Pygame dev 9
+      # Add true end game conditions - win and lose
+      if player_character.detect_collision(enemy_0):
+        is_game_over = True
+        did_win = False
+        text = font.render('You SUCK! wah wah!', True, WHITE_COLOR)
+        self.game_screen.blit(text, (220, 350))
+        pygame.display.update()
+        clock.tick(1)
+        break
+      elif player_character.detect_collision(treasure):
+        is_game_over = True
+        did_win = True
+        text = font.render('Whoo, you rule BITCH!', True, WHITE_COLOR)
+        self.game_screen.blit(text, (200, 350))
+        pygame.display.update()
+        clock.tick(1)
+        break
+      # Pygame has some really optimized collision fuctions on board you should use when you have many potential collisions to track
 
       pygame.display.update()
       clock.tick(self.TICK_RATE)
+
+    if did_win:
+      self.run_game_loop()
+    else:
+      return
 
 # Pygame dev 5
 # Implement game classes - generic game obj class
@@ -89,18 +129,54 @@ class PlayerCharacter(GameObject):
   SPEED = 10
 
   def __inti__(self, image_path, x, y, width, height):
-    super().__init__(omage_path, x, y, width, height)
+    super().__init__(image_path, x, y, width, height)
 
-  def move(self, direction):
+  def move(self, direction, max_height):
     if direction > 0:
       self.y_pos -= self.SPEED
     elif direction < 0:
       self.y_pos += self.SPEED
 
+    if self.y_pos >= max_height - 20:
+      self.y_pos = max_height - 20
+
+  # Pygame dev 8
+  # Implement collision detection with enemies and treasure
+
+  def detect_collision(self, other_body):
+    if self.y_pos > other_body.y_pos + other_body.height:
+      return False
+    elif self.y_pos + self.height < other_body.y_pos:
+      return False
+
+    if self.x_pos > other_body.x_pos + other_body.width:
+      return False
+    elif self.x_pos + self.width < other_body.x_pos:
+      return False
+
+    return True
+
+# Pygame dev 7
+# Implement game classes - enemy character and bounds checking
+
+class EnemyCharacter(GameObject):
+
+  SPEED = 10
+
+  def __inti__(self, image_path, x, y, width, height):
+    super().__init__(image_path, x, y, width, height)
+
+  def move(self, max_width):
+    if self.x_pos <= 20:
+      self.SPEED = abs(self.SPEED)
+    elif self.x_pos >= max_width - 20:
+      self.SPEED = -abs(self.SPEED)
+    self.x_pos += self.SPEED
+
 
 pygame.init()
 
-new_game = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
+new_game = Game('background.png', SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
 new_game.run_game_loop()
 
 
